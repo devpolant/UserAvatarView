1edb38498a3b5adfa18b2bbc4959ac5c7658bb12
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2019-02-03 12:51:38 +0200

    Add empty example

---
+++ Example/UserAvatarViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserAvatarViewExample
+//
+//  Created by Anton Poltoratskyi on 03.02.2019.
+//  Copyright © 2019 Anton Poltoratskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
