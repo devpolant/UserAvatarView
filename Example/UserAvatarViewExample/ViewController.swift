aecdab4119d9e252d9113b9be521cf8cc92a6cef
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2019-02-03 13:19:36 +0200

    Implemented Example

--- Example/UserAvatarViewExample/ViewController.swift
+++ Example/UserAvatarViewExample/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import UserAvatarView
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Views
+    
+    @IBOutlet weak var avatarView: UserAvatarView!
+    
+    @IBOutlet weak var radiusSlider: UISlider!
+    
+    @IBOutlet weak var radiusLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var angleLabel: UILabel!
+    
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let initialRadius = 24
+        radiusSlider.value = Float(initialRadius)
+        avatarView.statusRadius = CGFloat(initialRadius)
+        radiusLabel.text = "Radius: \(initialRadius)"
+        
+        let initialAngle = 45
+        slider.value = Float(initialAngle)
+        avatarView.angle = initialAngle.degreesToRadians
+        angleLabel.text = "Angle: \(initialAngle)"
+        
+        avatarView.imageView.backgroundColor = .green
+        avatarView.update(.color(.red))
     }
-
-
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func actionRadiusSliderValueChanged(_ sender: UISlider) {
+        let radius = Int(sender.value)
+        avatarView.statusRadius = CGFloat(radius)
+        radiusLabel.text = "Radius: \(radius)"
+    }
+    
+    @IBAction func actionSliderValueChanged(_ sender: UISlider) {
+        let angle = Int(sender.value)
+        avatarView.angle = CGFloat(angle.degreesToRadians)
+        angleLabel.text = "Angle: \(angle)"
+    }
 }
 
+extension BinaryInteger {
+    var degreesToRadians: CGFloat { return CGFloat(Int(self)) * .pi / 180 }
+}
+
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
+}
